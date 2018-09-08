c490df6bd217ca0de0e9b89dc89a5bffd8ba6b57
Yoda <indefinitedevil@nokvir.net>
2018-09-08 17:44:00 +0100

    Update security.php

--- security.php
+++ security.php
@@ -5,9 +5,9 @@ include('header.php');
 <audio id="security" src="audio/security.mp3" preload="auto"></audio>
 <div class="audio-control" data-id="security">&#9654;</div>
 <p>Welcome to Aztl<span class="kreoleac ancient">a</span><span class="latin">á</span>n.</p>
-<p>Due to previous incursions causing further damage, we have had to isolate you until your identity and intent can be confirmed.</p>
-<p>The <?php stargate(); ?> only allows inward travel from your homeworld. It is not configured to allow outward travel.</p>
-<p>The <?php rings(); ?> will allow you to travel within the local area. Access to certain areas is restricted.</p>
+<p>Testing phase is now complete.</p>
+<p>You have been successful. Further access will be provided to aid in repairs pending authorisation.</p>
+<p>Authorisation must be provided by the highest ranking crewmember aboard.</p>
 </div>
 <?php
 include('footer.php');
