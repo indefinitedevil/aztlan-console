a1b5ca0af6524b4678ed64ebf08a0fd98d787497
Yoda <indefinitedevil@nokvir.net>
2018-09-09 09:48:03 +0100

    Update security.php

--- security.php
+++ security.php
@@ -6,8 +6,7 @@ include('header.php');
 <div class="audio-control" data-id="security">&#9654;</div>
 <p>Welcome to Aztl<span class="kreoleac ancient">a</span><span class="latin">á</span>n.</p>
 <p>Testing phase is now complete.</p>
-<p>You have been successful. Further access will be provided to aid in repairs pending authorisation.</p>
-<p>Authorisation must be provided by the highest ranking crewmember aboard.</p>
+<p>Access to remaining areas requiring repair has been approved.</p>
   
 </div>
 <?php
